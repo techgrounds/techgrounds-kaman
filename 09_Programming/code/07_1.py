ce1ecaa9df46257c1fcc5cbcd0f0433e602560a2
kaman-codes <kaman010@gmail.com>
2023-08-14 22:44:07 +0200

    update

---
+++ 09_Programming/code/07_1.py
@@ -0,0 +1,11 @@
+"""
+Exercise 1:
+Create a new script.
+Create a variable that contains a list of five names.
+Loop over the list using a for loop. Print every individual name in the list on a new line.
+"""
+
+fish_list = [ "swordfish","shark","tuna","red snapper","giant trevally"]
+
+for i in range(len(fish_list)):
+    print(fish_list[i])
---
+++ 09_Programming/code/07_2.py
@@ -0,0 +1,23 @@
+"""
+Exercise 2:
+Create a new script.
+Create a list of five integers.
+Use a for loop to do the following for every item in the list:
+Print the value of that item added to the value of the next item in the list. 
+If it is the last item, add it to the value of the first item instead (since there is no next item).
+[image]
+The first result above is created by adding 9 and 80. The second result is created by adding 80 and 16, etc. 
+The last result is created by adding 35 and 9.
+"""
+
+my_list = [8,3,20,6,12]
+
+for i in range(len(my_list)):
+
+# tel de 2 getallen bij elkaar op tot dat i bij de laatste is geeindigd.
+    if i != len(my_list) - 1:
+        print( my_list[i] + my_list[i + 1])
+    else:
+    #  tel de laaste op bij de eerste.
+        print( my_list[i] + my_list[0])
+    
